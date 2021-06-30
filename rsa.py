a653371da0284a6592666fd3e627051fa2179b4f
Adrien RICHARD <richardadrien0@gmail.com>
2021-06-30 10:17:00 +0200

    added more functions to decrypt

--- rsa.py
+++ rsa.py
@@ -2,8 +2,9 @@ from primesieve import *
 from random import *
 import math
 import base64
+from textwrap import wrap
 
-def isPrime(x):
+def isPrime(x: int) -> bool:
     """
     returns if a number is prime or not
     """
@@ -16,7 +17,7 @@ def isPrime(x):
             return False
     return True
 
-def generateKeyFile(n, e, typ):
+def generateKeyFile(n: int, e: int, typ: str):
     """
     Generathe the files for the keys 
     the 3rd parameter is eiver "private" or "public" defining what key to generate
@@ -66,35 +67,142 @@ def generateKeys():
     generateKeyFile(n, e, "public")
     generateKeyFile(n, d, "private")
 
-def extractParamsFromKey(key) -> []:
+def extractParamsFromKey(key: str) -> []:
     """
     Extract the two parameters from the key
     the key is given in base 64
     """
     l = base64.b64decode(key).decode('ascii')
+    
     param1 = l.split('\n')[0]
     param2 = l.split('\n')[1]
+    #convert back to int
+    param1 = int(param1, 16)
+    param2 = int(param2, 16)
+    
     # print(param1,param2)
     return [param1,param2]
 
-def encode(keyFile):
+def writeToFile(file: str, text: str):
+    """
+    Write a string to a file
+    """
+    f = open(file, "w")
+    f.write(text)
+    f.close()
+
+def readFile(file: str) -> str:
+    """
+    read a file
+    """
+    f = open(file, "r")
+    content = f.read()
+    f.close()
+    return content
+
+def encode(keyFile, string):
     """
     Encode a file using the public key
     """
     print("Encoding message ...")
     print("Is public key file ok ?", checkKeyFile(keyFile,"public"))
 
-    f = open(keyFile)
-    extractParamsFromKey(f.readlines()[1]) # read the second line of the file and extract the param
+    if (checkKeyFile(keyFile,"public")):        
+        f = open(keyFile)
+        keyData = extractParamsFromKey(f.readlines()[1]) # read the second line of the file and extract the param
+        print("keydata", keyData)
+        
+        # transform the ascii string into a series of numbers
+        asciiToInt = ""
+        for char in string :
+            asciiToInt += str(ord(char))
+        print("ascii to int", asciiToInt)
+        
+        # calculate the block length
+        blocklen = len(str(keyData[0]))
+        print("block size is ",blocklen )
+        
+        # cut the ascii numbers into bloks of that length.
+        blocks = wrap(asciiToInt, blocklen)
+        # print(blocks)
+        
+        # make sur that every block is the corect length, overwise add padding
+        for i in range(len(blocks)):
+            if(len(str(blocks[i])) != blocklen):
+                print("adding padding")
+                blocks[i] = blocks[i].zfill(blocklen)
 
-def decode():
+        # crypt everyblock
+        tempCryptString = ""
+        for i in range(len(blocks)): 
+            tempCryptString += str(calculateCrypt(blocks[i], keyData[1], keyData[0]))
+        print("encrypted string :",tempCryptString)
+        # write the contentes to a file
+        writeToFile("encoded", tempCryptString)
+
+        # writeToFile("encoded", stringToHextoBase64(tempCryptString))
+    else: 
+        print("keyfile is incorrect")
+
+def calculateCrypt(asci: int, e: int, n: int) -> int:
+    """
+    Calculate the crypt int
+    """
+    return int(asci)^e % n
+
+def calculateDeCrypt(asci: int, d: int, n: int) -> int:
+    """
+    Calculate the decrypt int
+    """
+    return int(asci)^d % n
+
+def stringToHextoBase64(inputString: str) -> str: 
+    """
+    input = a string of numbers
+    Takee a string, transform it to int then to hex then to base64
+    """
+    inputString = str(inputString)
+    message = hex(int(inputString))
+    message_bytes = message.encode('ascii')
+    base64_bytes = base64.b64encode(message_bytes)
+    return str(base64_bytes.decode("ascii"))
+
+def base64ToHexToString(inputString: str) -> str:
+    """
+    input = a base64 string
+    Take the abse 64, make it  ahex then a string
+    """
+
+def decode(keyFile: str, string : str):
     """
     decode a file using the private key
     """
     print("Decoding message ...")
-    return
+    print("Is private key file ok ?", checkKeyFile(keyFile,"private"))
 
-def prime_factors(n):
+    if (checkKeyFile(keyFile,"private")):        
+        f = open(keyFile)
+        keyData = extractParamsFromKey(f.readlines()[1]) # read the second line of the file and extract the param
+        print("keydata", keyData)
+        
+        blocklen = len(str(keyData[0]))
+        print("block size is ",blocklen)
+        
+        # split the string into blocks
+        blocks = wrap(string, blocklen)
+        print(blocks)
+        
+        tmpDecoded = ""
+        for i in range(len(blocks)):    
+            tmpDecoded += str(calculateDeCrypt(str(blocks[i]), keyData[1], keyData[0]))
+        print(tmpDecoded)
+
+        
+    else: 
+        print("keyfile is incorrect")
+
+
+def prime_factors(n) -> []:
     """
     Get the prime factors for a number
     """
@@ -110,7 +218,7 @@ def prime_factors(n):
         factors.append(n)
     return factors
 
-def genED(nn):
+def genED(nn: int) -> []:
     """
     generate the E and D numbers
     """
@@ -145,7 +253,7 @@ def genED(nn):
             else:
                 print("e can not be equal to d")
 
-def checkKeyFile(file,typ):
+def checkKeyFile(file : str,typ : str) -> bool:
     """
     check if a key file is or is not valid.
     the 3rd parameter is eiver "private" or "public" defining what key to generate
@@ -171,4 +279,5 @@ def checkKeyFile(file,typ):
 
 # entry point
 generateKeys()
-encode("test.pub")
+encode("test.pub", "Hello World! I hope you are having a great day!")
+decode("test.priv", readFile("encoded"))
